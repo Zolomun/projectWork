b21c5cd4e145a44a76eb00c833659a4413cd7de6
Papp Zoltán <pappzoli880@gmail.com>
2023-11-24 12:18:00 +0100

    add chrome options

--- src/test/java/browser/DriverInitializer.java
+++ src/test/java/browser/DriverInitializer.java
@@ -8,15 +8,23 @@ import org.openqa.selenium.chrome.ChromeOptions;
 public class DriverInitializer {
 
 
-        public static WebDriver initDriver(BrowserType browserType) {
-            switch(browserType) {
-                case CHROME_SELENIUM_MGR:
-                    ChromeOptions options = new ChromeOptions();
-                    options.addArguments("--disable-blink-features=AutomationControlled");
-                    options.addArguments("--incognito");
-                    return new ChromeDriver(options);
-                default:
-                    throw new IllegalArgumentException("Incorrect browser type!");
-            }
+    public static WebDriver initDriver(BrowserType browserType) {
+        switch (browserType) {
+            case CHROME_SELENIUM_MGR:
+                ChromeOptions options = new ChromeOptions();
+                options.addArguments("--disable-blink-features=AutomationControlled");
+                options.addArguments("--incognito");
+                options.addArguments("--disable-gpu");
+                options.addArguments("--ignore-certificate-errors");
+                options.addArguments("--window-size=1920,1080");
+                options.addArguments("--allow-running-insecure-content");
+                options.addArguments("--start-maximized");
+                options.addArguments("--no-sandbox");
+                options.addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36");
+                options.addArguments("--headless");
+                return new ChromeDriver(options);
+            default:
+                throw new IllegalArgumentException("Incorrect browser type!");
         }
     }
+}
